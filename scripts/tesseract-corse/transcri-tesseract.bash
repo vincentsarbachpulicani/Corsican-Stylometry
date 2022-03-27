96d6b491b4984e08756a705de019886a36afcfa8
vincentsarbachpulicani <vincent.sarbach@laposte.net>
2022-03-27 16:45:21 +0200

    Update readme et ajout sorted_images

--- scripts/tesseract-corse/transcri-tesseract.bash
+++ scripts/tesseract-corse/transcri-tesseract.bash
@@ -1,6 +1,11 @@
 pdftoppm -r 300 -tiff *.pdf document
 for f in *.tif; do
+<<<<<<< HEAD
+	tesseract $f $(basename $f .tiff) -l fra+cos+ita
+	tesseract $f $(basename $f .tiff) -l fra+cos+ita alto
+=======
 	tesseract $f $f -l fra+cos+ita
 	tesseract $f $f -l fra+cos+ita alto
+>>>>>>> be120a2822fa251a6fe9d542bd9239736fc2132f
 done
 cat *.txt > document-total.txt
